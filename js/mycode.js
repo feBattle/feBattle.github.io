b18c83c366c576e826062b0d7cdf6d91d4a17d0f
DenisShapranov <surlych@gmail.com>
2015-10-24 02:30:47 +0300

    edit Zhenya and Denis

--- js/mycode.js
+++ js/mycode.js
@@ -100,7 +100,7 @@ $(document).ready(function() {
 					scaleControl: false,
 					overviewMapControl: false,
 					center: new google.maps.LatLng(50.4390449, 30.498883400000068),
-					styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]
+					styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#ffffff"},{"lightness":14},{"weight":1.3}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]
 				};
 				
 				map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -150,7 +150,7 @@ $(document).ready(function() {
 
 	/* скроллинг меню */
 
-		$('#nav-about').on('click', function(e){
+		$('#nav-about,#learn_more').on('click', function(e){
 			var header = $('.main_header').height();
 			var scrolling = ($('.about_us').offset().top)-header;
 			e.preventDefault();
@@ -176,7 +176,7 @@ $(document).ready(function() {
 
 		$('#nav-feedback').on('click', function(e){
 		var header = $('.main_header').height();
-		var scrolling = ($('.feedback_sec').offset().top)-header;
+		var scrolling = ($('#feedback_sec').offset().top)-header;
 		e.preventDefault();
 
 		$('html,body').stop(true).animate({scrollTop: scrolling},500,function(){	})
